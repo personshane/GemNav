bc52d07a616a28ab23e1c1b295adf50b7a0d2bbe
personshane <personshane@yahoo.com>
2025-11-22 12:09:04 -0700

    MP-A07-A: Android project root scaffolding - settings.gradle.kts and build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,32 @@
+// Project: GemNav
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://repo.here.com/artifactory/open-location-platform") }
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
